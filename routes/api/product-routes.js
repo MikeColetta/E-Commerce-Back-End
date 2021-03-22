994003c99dbc5cfe1ac556b5db73e662e5bcf442
Mike Coletta <coletta.mike@gmail.com>
2021-03-22 12:49:27 -0700

    Added view categories and tag data in get routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category, as: 'category' }, { model: Tag, through: ProductTag, as: 'tag' }]
+      include: [{ model: Category, as: 'category' }, { model: Tag, through: ProductTag }]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category, as: 'category' }]
+      include: [{ model: Category, as: 'category' }, { model: Tag, through: ProductTag }]
     });
 
     if (!productData) {
@@ -110,8 +110,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that ID!'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
